17fbca50c6b6c59d3a5f35e4014328499c9ae3ce
Hamed Montazeri <hamed.mon@gmail.com>
2020-08-26 17:32:54 +0200

    some changes for stream option

--- src/recorder/recorder.ts
+++ src/recorder/recorder.ts
@@ -1,4 +1,4 @@
-import { compress, encodePCM, encodeWAV } from '../transform/transform';
+import { compress, encodePCM, encodeWAV } from "../transform/transform";
 
 declare let window: any;
 declare let Math: any;
@@ -7,47 +7,48 @@ declare let Promise: any;
 
 // 构造函数参数格式
 interface recorderConfig {
-    sampleBits?: number,        // 采样位数
-    sampleRate?: number,        // 采样率
-    numChannels?: number,       // 声道数
-    compiling?: boolean,        // 是否边录边播
+    sampleBits?: number; // 采样位数
+    sampleRate?: number; // 采样率
+    numChannels?: number; // 声道数
+    compiling?: boolean; // 是否边录边播
+    stream?: MediaStream;
 }
 
 export default class Recorder {
     private context: any;
-    protected config: recorderConfig;               // 配置
+    protected config: recorderConfig; // 配置
     private analyser: any;
-    private size: number = 0;                       // 录音文件总长度
-    private lBuffer: Array<Float32Array> = [];      // pcm音频数据搜集器(左声道)
-    private rBuffer: Array<Float32Array> = [];      // pcm音频数据搜集器(右声道)
-    private PCM: any;                               // 最终的PCM数据缓存，避免多次encode
-    private tempPCM: Array<DataView> = [];          // 边录边转时临时存放pcm的
+    private size: number = 0; // 录音文件总长度
+    private lBuffer: Array<Float32Array> = []; // pcm音频数据搜集器(左声道)
+    private rBuffer: Array<Float32Array> = []; // pcm音频数据搜集器(右声道)
+    private PCM: any; // 最终的PCM数据缓存，避免多次encode
+    private tempPCM: Array<DataView> = []; // 边录边转时临时存放pcm的
     private audioInput: any;
-    protected inputSampleRate: number;              // 输入采样率
-    protected inputSampleBits: number = 16;         // 输入采样位数
-    protected outputSampleRate: number;             // 输出采样率
-    protected oututSampleBits: number;              // 输出采样位数
-    private source: any;                            // 音频输入
+    protected inputSampleRate: number; // 输入采样率
+    protected inputSampleBits: number = 16; // 输入采样位数
+    protected outputSampleRate: number; // 输出采样率
+    protected oututSampleBits: number; // 输出采样位数
+    private source: any; // 音频输入
     private recorder: any;
-    private stream: any;                            // 流
-    protected littleEdian: boolean;                 // 是否是小端字节序
-    protected fileSize: number = 0;                 // 录音大小，byte为单位
-    protected duration: number = 0;                 // 录音时长
-    private needRecord: boolean = true;             // 由于safari问题，导致使用该方案代替disconnect/connect方案
+    private stream: any; // 流
+    protected littleEdian: boolean; // 是否是小端字节序
+    protected fileSize: number = 0; // 录音大小，byte为单位
+    protected duration: number = 0; // 录音时长
+    private needRecord: boolean = true; // 由于safari问题，导致使用该方案代替disconnect/connect方案
     // 正在录音时间，参数是已经录了多少时间了
     public onprocess: (duration: number) => void;
     // onprocess 替代函数，保持原来的 onprocess 向下兼容
     public onprogress: (payload: {
-        duration: number,
-        fileSize: number,
-        vol: number,
+        duration: number;
+        fileSize: number;
+        vol: number;
         // data: Array<DataView>,      // 当前存储的所有录音数据
     }) => void;
-    public onplay: () => void;                  // 音频播放回调
-    public onpauseplay: () => void;             // 音频暂停回调
-    public onresumeplay: () => void;            // 音频恢复播放回调
-    public onstopplay: () => void;              // 音频停止播放回调
-    public onplayend: () => void;               // 音频正常播放结束
+    public onplay: () => void; // 音频播放回调
+    public onpauseplay: () => void; // 音频暂停回调
+    public onresumeplay: () => void; // 音频恢复播放回调
+    public onstopplay: () => void; // 音频停止播放回调
+    public onplayend: () => void; // 音频正常播放结束
 
     /**
      * @param {Object} options 包含以下三个参数：
@@ -59,35 +60,51 @@ export default class Recorder {
         // 临时audioContext，为了获取输入采样率的
         let context = new (window.AudioContext || window.webkitAudioContext)();
 
-        this.inputSampleRate = context.sampleRate;     // 获取当前输入的采样率
+        this.inputSampleRate = context.sampleRate; // 获取当前输入的采样率
 
         // 设置输出配置
         this.setNewOption(options);
 
         // 判断端字节序
-        this.littleEdian = (function() {
+        this.littleEdian = (function () {
             let buffer = new ArrayBuffer(2);
             new DataView(buffer).setInt16(0, 256, true);
             return new Int16Array(buffer)[0] === 256;
         })();
         // 兼容 getUserMedia
-        Recorder.initUserMedia();
+        // Recorder.initUserMedia();
     }
 
     protected setNewOption(options: recorderConfig = {}) {
         this.config = {
+            stream: options.stream,
             // 采样数位 8, 16
-            sampleBits: ~[8, 16].indexOf(options.sampleBits) ? options.sampleBits : 16,
+            sampleBits: ~[8, 16].indexOf(options.sampleBits)
+                ? options.sampleBits
+                : 16,
             // 采样率
-            sampleRate: ~[8000, 11025, 16000, 22050, 24000, 44100, 48000].indexOf(options.sampleRate) ? options.sampleRate : this.inputSampleRate,
+            sampleRate: ~[
+                8000,
+                11025,
+                16000,
+                22050,
+                24000,
+                44100,
+                48000,
+            ].indexOf(options.sampleRate)
+                ? options.sampleRate
+                : this.inputSampleRate,
             // 声道数，1或2
-            numChannels: ~[1, 2].indexOf(options.numChannels) ? options.numChannels : 1,
+            numChannels: ~[1, 2].indexOf(options.numChannels)
+                ? options.numChannels
+                : 1,
             // 是否需要边录边转，默认关闭，后期使用web worker
             // compiling: !!options.compiling || false,   // 先移除
         };
+
         // 设置采样的参数
-        this.outputSampleRate = this.config.sampleRate;     // 输出采样率
-        this.oututSampleBits = this.config.sampleBits;      // 输出采样数位 8, 16
+        this.outputSampleRate = this.config.sampleRate; // 输出采样率
+        this.oututSampleBits = this.config.sampleBits; // 输出采样数位 8, 16
     }
 
     /**
@@ -101,27 +118,25 @@ export default class Recorder {
             // 关闭先前的录音实例，因为前次的实例会缓存少量前次的录音数据
             this.destroyRecord();
         }
-        // 初始化
-        this.initRecorder();
 
-        return navigator.mediaDevices.getUserMedia({
-            audio: true
-        }).then(stream => {
-            // audioInput表示音频源节点
-            // stream是通过navigator.getUserMedia获取的外部（如麦克风）stream音频输出，对于这就是输入
-            this.audioInput = this.context.createMediaStreamSource(stream);
-            this.stream = stream;
-        }/* 报错丢给外部使用者catch，后期可在此处增加建议性提示
-            , error => {
-            // 抛出异常
-            Recorder.throwError(error.name + " : " + error.message);
-        } */).then(() => {
-            // audioInput 为声音源，连接到处理节点 recorder
-            this.audioInput.connect(this.analyser);
-            this.analyser.connect(this.recorder);
-            // this.audioInput.connect(this.recorder);
-            // 处理节点 recorder 连接到扬声器
-            this.recorder.connect(this.context.destination);
+        return new Promise((resolve, reject) => {
+            try {
+                // audioInput表示音频源节点
+                // stream是通过navigator.getUserMedia获取的外部（如麦克风）stream音频输出，对于这就是输入
+                this.stream = this.config.stream;
+                this.audioInput = this.context.createMediaStreamSource(
+                    this.config.stream
+                );
+                // audioInput 为声音源，连接到处理节点 recorder
+                this.audioInput.connect(this.analyser);
+                this.analyser.connect(this.recorder);
+                // this.audioInput.connect(this.recorder);
+                // 处理节点 recorder 连接到扬声器
+                this.recorder.connect(this.context.destination);
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
@@ -204,7 +219,7 @@ export default class Recorder {
      */
     private flat() {
         let lData = null,
-            rData = new Float32Array(0);    // 右声道默认为0
+            rData = new Float32Array(0); // 右声道默认为0
 
         // 创建存放数据的容器
         if (1 === this.config.numChannels) {
@@ -232,7 +247,7 @@ export default class Recorder {
 
         return {
             left: lData,
-            right: rData
+            right: rData,
         };
     }
 
@@ -245,16 +260,22 @@ export default class Recorder {
 
         this.context = new (window.AudioContext || window.webkitAudioContext)();
 
-        this.analyser = this.context.createAnalyser();  // 录音分析节点
-        this.analyser.fftSize = 2048;                   // 表示存储频域的大小
+        this.analyser = this.context.createAnalyser(); // 录音分析节点
+        this.analyser.fftSize = 2048; // 表示存储频域的大小
 
         // 第一个参数表示收集采样的大小，采集完这么多后会触发 onaudioprocess 接口一次，该值一般为1024,2048,4096等，一般就设置为4096
         // 第二，三个参数分别是输入的声道数和输出的声道数，保持一致即可。
-        let createScript = this.context.createScriptProcessor || this.context.createJavaScriptNode;
-        this.recorder = createScript.apply(this.context, [4096, this.config.numChannels, this.config.numChannels]);
+        let createScript =
+            this.context.createScriptProcessor ||
+            this.context.createJavaScriptNode;
+        this.recorder = createScript.apply(this.context, [
+            4096,
+            this.config.numChannels,
+            this.config.numChannels,
+        ]);
 
         // 音频采集
-        this.recorder.onaudioprocess = e => {
+        this.recorder.onaudioprocess = (e) => {
             if (!this.needRecord) {
                 return;
             }
@@ -262,7 +283,7 @@ export default class Recorder {
             // getChannelData返回Float32Array类型的pcm数据
             let lData = e.inputBuffer.getChannelData(0),
                 rData = null,
-                vol = 0;        // 音量百分比
+                vol = 0; // 音量百分比
 
             this.lBuffer.push(new Float32Array(lData));
 
@@ -284,9 +305,16 @@ export default class Recorder {
             //     // 计算录音大小
             //     this.fileSize = pcm.byteLength * this.tempPCM.length;
             // } else {
-                // 计算录音大小
-                this.fileSize = Math.floor(this.size / Math.max( this.inputSampleRate / this.outputSampleRate, 1))
-                    * (this.oututSampleBits / 8)
+            // 计算录音大小
+            this.fileSize =
+                Math.floor(
+                    this.size /
+                        Math.max(
+                            this.inputSampleRate / this.outputSampleRate,
+                            1
+                        )
+                ) *
+                (this.oututSampleBits / 8);
             // }
             // 为何此处计算大小需要分开计算。原因是先录后转时，是将所有数据一起处理，边录边转是单个 4096 处理，
             // 有小数位的偏差。
@@ -298,13 +326,14 @@ export default class Recorder {
             // 录音时长回调
             this.onprocess && this.onprocess(this.duration);
             // 录音时长及响度回调
-            this.onprogress && this.onprogress({
-                duration: this.duration,
-                fileSize: this.fileSize,
-                vol,
-                // data: this.tempPCM,     // 当前所有的pcm数据，调用者控制增量
-            });
-        }
+            this.onprogress &&
+                this.onprogress({
+                    duration: this.duration,
+                    fileSize: this.fileSize,
+                    vol,
+                    // data: this.tempPCM,     // 当前所有的pcm数据，调用者控制增量
+                });
+        };
     }
 
     /**
@@ -314,7 +343,7 @@ export default class Recorder {
      */
     private stopStream() {
         if (this.stream && this.stream.getTracks) {
-            this.stream.getTracks().forEach(track => track.stop());
+            this.stream.getTracks().forEach((track) => track.stop());
             this.stream = null;
         }
     }
@@ -324,7 +353,11 @@ export default class Recorder {
      * 如firefox 30 等低版本浏览器没有 close方法
      */
     private closeAudioContext() {
-        if (this.context && this.context.close && this.context.state !== 'closed') {
+        if (
+            this.context &&
+            this.context.close &&
+            this.context.state !== "closed"
+        ) {
             return this.context.close();
         } else {
             return new Promise((resolve) => {
@@ -340,17 +373,22 @@ export default class Recorder {
         }
 
         if (navigator.mediaDevices.getUserMedia === undefined) {
-            navigator.mediaDevices.getUserMedia = function(constraints) {
-                let getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+            navigator.mediaDevices.getUserMedia = function (constraints) {
+                let getUserMedia =
+                    navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia;
 
                 if (!getUserMedia) {
-                    return Promise.reject(new Error('浏览器不支持 getUserMedia !'));
+                    return Promise.reject(
+                        new Error("浏览器不支持 getUserMedia !")
+                    );
                 }
 
-                return new Promise(function(resolve, reject) {
+                return new Promise(function (resolve, reject) {
                     getUserMedia.call(navigator, constraints, resolve, reject);
                 });
-            }
+            };
         }
     }
 
@@ -365,10 +403,14 @@ export default class Recorder {
         let lBuffer = new Float32Array(lData),
             rBuffer = new Float32Array(rData);
 
-        let data = compress({
-            left: lBuffer,
-            right: rBuffer,
-        }, this.inputSampleRate, this.outputSampleRate);
+        let data = compress(
+            {
+                left: lBuffer,
+                right: rBuffer,
+            },
+            this.inputSampleRate,
+            this.outputSampleRate
+        );
 
         return encodePCM(data, this.oututSampleBits, this.littleEdian);
     }
@@ -376,8 +418,10 @@ export default class Recorder {
     static getPermission(): Promise<{}> {
         this.initUserMedia();
 
-        return navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
-            stream.getTracks().forEach(track => track.stop());
-        });
+        return navigator.mediaDevices
+            .getUserMedia({ audio: true })
+            .then((stream) => {
+                stream.getTracks().forEach((track) => track.stop());
+            });
     }
 }
